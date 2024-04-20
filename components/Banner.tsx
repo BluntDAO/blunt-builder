9c6c5429ff1c20e33d24c313e14421152c52ebec
Kuro Denjiro <kurodenjiro1@gmail.com>
2024-04-21 02:10:40 +0700

    Update Banner.tsx

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -4,8 +4,7 @@ export default function Banner() {
   return (
     <div className="w-full flex flex-col justify-center items-center">
       <div className="px-4 pt-2 pb-1 text-white font-bold bg-dark w-full justify-center items-center text-center flex">
-        Collective Nouns: A Community-Curated Daily Auction on BASE in Support
-        of The Yellow Collective
+      Regular auctions start 4/20 at 4:20 PM EST
       </div>
       <div className="w-full h-[16px] relative mb-2 -translate-y-0.5">
         <Image src="/black-drip.png" fill={true} alt="" />
