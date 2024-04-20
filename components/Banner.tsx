7a1e982c653b5be367464128163023353a1d7627
Kuro Denjiro <kurodenjiro1@gmail.com>
2024-04-21 02:53:36 +0700

    Update Banner.tsx

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -4,7 +4,7 @@ export default function Banner() {
   return (
     <div className="w-full flex flex-col justify-center items-center">
       <div className="px-4 pt-2 pb-1 text-white font-bold bg-dark w-full justify-center items-center text-center flex">
-      Regular auctions start 4/20 at 4:20 PM EST
+      OUNCE: the first global sesh fund, live on BASE
       </div>
       <div className="w-full h-[16px] relative mb-2 -translate-y-0.5">
         <Image src="/black-drip.png" fill={true} alt="" />
