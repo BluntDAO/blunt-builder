ebd585d861a4dc5c915cf58f8d0e7e5cdc6dc46a
Kuro Denjiro <kurodenjiro1@gmail.com>
2024-04-21 02:18:22 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -39,6 +39,10 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
               property="og:description"
               content="One Ounce, every day, forever. BluntsDAO is building the unlimited sesh fund to onboard the next million users to Web3, 1 blunt at a time. CC0 on Base."
             />
+            <link rel="icon" href="/favicon.ico" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
             <meta property="og:type" content="website" />
             <meta property="og:url" content={`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/`} />
             <meta
